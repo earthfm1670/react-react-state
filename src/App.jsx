c8e06dee2aebbd19107713ae29255995a4dddb6c
Werapol Jaranai <r5r5wa55@hotmail.com>
2024-10-23 14:17:38 +0700

    feat:add function handle button we

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import "./App.css";
-
 function App() {
+  let [greetingMes, setGreetingMes] = useState("Greeting Message");
+  const handleThai = () => {
+    setGreetingMes("สวัสดี");
+  };
+  const handleEng = () => {
+    setGreetingMes("Hi!");
+  };
+  const handleJapan = () => {
+    setGreetingMes("你好!");
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      {" "}
+      <div className="greeting-container">{greetingMes}</div>{" "}
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
-      </div>
+        {" "}
+        <button onClick={handleThai}>สวัสดี</button>{" "}
+        <button onClick={handleEng}>Hi!</button>{" "}
+        <button onClick={handleJapan}>你好!</button>{" "}
+      </div>{" "}
     </div>
   );
 }
-
 export default App;
