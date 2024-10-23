c2ea6b3ef4244a6b21ef4c9570c339fa5db13458
Tonkla <Apisit378819@gmail.com>
2024-10-23 14:07:44 +0700

    feat:add jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import "./App.css";
+import { useState } from 'react';
 
 function App() {
+  const [counter,setCounter] = useState("Greeting Message")
+  const clickA = () => {
+    setCounter("สวัสดี!")
+  }
+  const clickB = () => {
+    setCounter("Hi!")
+  }
+  const clickC = () => {
+    setCounter("你好!")
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{counter}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={clickA} >สวัสดี!</button>
+        <button onClick={clickB} >Hi!</button>
+        <button onClick={clickC} >你好!</button>
       </div>
     </div>
   );
